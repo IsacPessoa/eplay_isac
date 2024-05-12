5b1e7a46e6b10bf19d6c45a76b5f2249c5a29625
Isac Pessoa <isacdavidpessoa@hotmail.com>
2024-05-12 19:15:20 -0300

    aplicando estilização a componentes

--- src/components/Prouct/index.tsx
+++ src/components/Prouct/index.tsx
@@ -1,19 +1,35 @@
 import Tag from '../Tag'
 
-import { Card, Descricao, Titulo } from './styles'
+import { Card, Descricao, Titulo, Infos } from './styles'
 
-const Product = () => (
+type Props = {
+  title: string
+  category: string
+  system: string
+  description: string
+  infos: string[]
+  image: string
+}
+
+const Product = ({
+  title,
+  category,
+  system,
+  description,
+  infos,
+  image
+}: Props) => (
   <Card>
-    <img src="//placehold.it/222x250" />
-    <Titulo>Nome do Jogo</Titulo>
-    <Tag>Categoria</Tag>
-    <Tag>Windows</Tag>
-    <Descricao>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque nesciunt,
-      aliquam fugit eligendi magni nostrum pariatur, excepturi incidunt commodi
-      aperiam qui ea, unde nulla. Perspiciatis nemo mollitia temporibus neque
-      exercitationem.
-    </Descricao>
+    <img src={image} alt={title} />
+    <Infos>
+      {infos.map((info) => (
+        <Tag key={info}>{info}</Tag>
+      ))}
+    </Infos>
+    <Titulo>{title}</Titulo>
+    <Tag>{category}</Tag>
+    <Tag>{system}</Tag>
+    <Descricao>{description}</Descricao>
   </Card>
 )
 
